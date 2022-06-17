b5d540b861612fca5beb425bf93cb416e2418fc6
Pavel Sobolev <pavelsobolev1991@gmail.com>
2022-06-17 12:33:28 +0300

    getTopic,getAnswer,showResults

--- View.js
+++ View.js
@@ -1,7 +1,46 @@
-class View {
-  constructor() {
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+class showQuiz {
+  getTopics(arrayOfTopics) {
+    return new Promise((res) => {
+      console.clear();
+      console.log('💖QUIZ WITH LOVE FROM ELBRUS💖');
+      console.log('LET\'S GO!');
+      console.log('\n');
+      console.log('🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴🔴');
+      console.log('\n');
+      console.log('Темы вопросов:');
+
+      console.log(arrayOfTopics.map((topic, index) => `${index + 1}. ${topic}`).join('\n'));
+
+      readline.question('\nВыберите тему, затем введите число или напишите \'Отмена\'): ', (chosenTopic) => {
+        resolve(chosenTopic);
+        // readline.close()
+      });
+    });
   }
+
+  getAnswer(answer) {
+    return new Promise((resolve) => {
+      console.log('❓❔❓❔❓❔❓❔❓❔❓❔❓❔❓❔');
+      readline.question('\n:восклицательный_знак:Введите ответ: ', (userAnswer) => {
+        resolve(userAnswer);
+      });
+    });
+  }
+
+  showResults(result) {
+    console.log(' 🥳 🎉 🎊🥳 🎉 🎊🥳 🎉 🎊', `\nПОЗДРАВЛЯЮ!\n
+ВИКТОРИНА ЗАКОНЧЕНА.`, ' 🥳 🎉 🎊🥳 🎉 🎊🥳 🎉 🎊');
+    console.log('👇', `\nТВОЙ РЕЗУЛЬТАТ: ${result} баллов!`, '💪💪💪💪💪💪💪💪💪💪');
+    readline.close();
+  }
+
+
+  
 }
 
-module.exports = View
+module.exports = View;

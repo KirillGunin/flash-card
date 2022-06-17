6ed55c66ef75e32428cce20ac6b54083f707f191
Ulyana <ulechka1996@icloud.com>
2022-06-17 11:27:32 +0300

    getThemes method

--- Model.js
+++ Model.js
@@ -1,7 +1,35 @@
-class Model {
-  constructor() {
+const fs = require('fs');
 
+class Model {
+  constructor(path) {
+    this.path = path;
+    this.arrThemes = []
   }
+  
+  getThemes () {
+    return new Promise((res, rej) => {
+      fs.readdir('./topics', ( err, data) => {
+        if(err) {
+          rej('Не удалось прочитать темы')
+        } else {
+          this.arrThemes = data;
+          res(data.map((el) => this.getFirstWord(el)))
+          // console.log(data);
+        }
+      })
+      
+    })
+  }
+
+  getFirstWord (word) {
+    return word.split('_')[0]
+  }
+
+
 }
+
+// const model = new Model ('./topics')
+// model.getThemes()
+//   .then((data)=> console.log(data))
 
 module.exports = Model
